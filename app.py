e2d48dc43271d6e99ffa0e05baf7a4bac732887d
vinaynaman <vinay.namani@katonic.ai>
2022-06-22 14:13:47 +0530

    updated some patches

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import numpy as np
 import pandas as pd
 import base64
 import time
+from io import BytesIO
+from typing import List
 
 timestr = time.strftime("%Y%m%d-%H%M%S")
 
@@ -18,8 +20,8 @@ def scrape_data(number_of_pages:int, keyword:str):
     keyword = keyword.lower()
     header = []
     content = []
-    st.markdown("Downloading the Data. Please wait..")
-    st.markdown("This might take few minutes. Grab a cup of coffee and wait..")
+    st.info(f"Number of pages found {number_of_pages}")
+    st.markdown("Downloading the Data. Grab a cup of coffee and wait.")
     print("Downloading Data. Please Wait..")
 
     my_bar = st.progress(0)
@@ -28,27 +30,25 @@ def scrape_data(number_of_pages:int, keyword:str):
 
     for i, j in zip(pages, sessions):
         my_bar.progress(j)
-        if i % 2 == 0:
-            print("#", end="")
         url = f"https://klinikradar.de/{keyword}/kliniken/{i}/"
         uclient = ureq(url)
         page = uclient.read()
         uclient.close()
         html_page = bs(page, "html.parser")
-    for div in html_page.findAll('h3', {"class": "serp-card-heading"}):
-        header.append(div.find('a').contents[0])
-    for div in html_page.find_all("div", {"class": "serp-card-highlight-subline"}):
-        head = div.contents[0]
-        if not head == "Patientenbefragung der Techniker Krankenkasse":
-            content.append(head)
+        for div in html_page.findAll('h3', {"class": "serp-card-heading"}):
+            header.append(div.find('a').contents[0])
+        for div in html_page.find_all("div", {"class": "serp-card-highlight-subline"}):
+            head = div.contents[0]
+            if not head == "Patientenbefragung der Techniker Krankenkasse":
+                content.append(head)
     return header, content
 
-def structuring_data_to_excel(header_data, content_data):
+def structuring_data_to_excel(header_data: List, content_data: List):
     content1 = []
     for i in range(0, len(content_data), 2):
         content1.append(content_data[i])
     content2 = [i for i in content if i not in content1]
-    df = pd.DataFrame({"content1": header_data, "content2":content1, "content3": content2})
+    df = pd.DataFrame({"S.no":range(1, len(content1)+1), "content1": header_data, "content2":content1, "content3": content2})
     return df
 
 st.header("Unknow Application")
@@ -68,22 +68,28 @@ class FileDownloader(object):
     def download(self):
         b64 = base64.b64encode(self.data.encode()).decode()
         new_filename = "{}_{}.{}".format(self.filename,timestr,self.file_ext)
-        st.markdown("#### Download File ###")
-        href = f'<a href="data:file/{self.file_ext};base64,{b64}" download="{new_filename}">Click Here!!</a>'
+        st.success("#### Data Downloaded Successfully ####")
+        href = f'<a href="data:file/{self.file_ext};base64,{b64}" download="{new_filename}">Click Here to download the file.!!</a>'
         st.markdown(href,unsafe_allow_html=True)
         
 if download:
     oepning_url = f"https://klinikradar.de/{search_term}/kliniken/1/"
-    uclient = ureq(oepning_url)
-    page = uclient.read()
-    uclient.close()
-    html_page = bs(page, "html.parser")
 
-    number_of_pages = find_max_pages(html_page)
+    r = requests.head(oepning_url)
 
-    header, content = scrape_data(number_of_pages, search_term)
+    if r.status_code == 200:
+        uclient = ureq(oepning_url)
+        page = uclient.read()
+        uclient.close()
+        html_page = bs(page, "html.parser")
 
-    df = structuring_data_to_excel(header, content)
+        number_of_pages = find_max_pages(html_page)
 
-    download = FileDownloader(df.to_csv(),filename = search_term,file_ext='xlsx').download()
+        header, content = scrape_data(number_of_pages, search_term)
+
+        df = structuring_data_to_excel(header, content)
+
+        download = FileDownloader(df.to_csv(index = False),filename = search_term,file_ext='csv').download()
+    else:
+        st.error("Wrong Search keyword, Please enter correct Search keyword.")
 
